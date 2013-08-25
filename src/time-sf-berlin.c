c29884bd94897d7690d057203028a3de92d7c0e1
Stefan Thoss <stef@nthoss.de>
2013-08-24 20:09:03 -0700

    Replaced date with city name

--- src/time-sf-berlin.c
+++ src/time-sf-berlin.c
@@ -4,6 +4,9 @@
 	
 #define TITLE "Time SF Berlin"
 #define AUTHOR "Stefan Thoss"
+	
+#define CITY1 "San Francisco"
+#define CITY2 "Berlin"
 
 #define MY_UUID {0xf2, 0x09, 0xc2, 0x9a, 0x14, 0x04, 0x83, 0x19, 0xbb, 0xfb, 0xb1, 0x8c, 0x9a, 0x9a, 0x19, 0x0f}
 
@@ -11,6 +14,8 @@ PBL_APP_INFO(MY_UUID, TITLE, AUTHOR, 1, 0, DEFAULT_MENU_ICON, APP_INFO_WATCH_FAC
 
 Window window;
 
+TextLayer text_city1_layer;
+TextLayer text_city2_layer;
 TextLayer text_date1_layer;
 TextLayer text_date2_layer;
 TextLayer text_time1_layer;
@@ -25,7 +30,7 @@ void line_layer_update_callback(Layer *me, GContext* ctx) {
 
   graphics_draw_line(ctx, GPoint(8, 35), GPoint(144-16, 35));
 
-  graphics_draw_line(ctx, GPoint(8, 119), GPoint(144-16, 1119));
+  graphics_draw_line(ctx, GPoint(8, 119), GPoint(144-16, 119));
 }
 
 
@@ -36,16 +41,25 @@ void handle_init(AppContextRef ctx) {
   window_set_background_color(&window, GColorBlack);
 
   resource_init_current_app(&APP_RESOURCES);
+	
+  // 1st city
+  text_layer_init(&text_city1_layer, window.layer.frame);
+  text_layer_set_text_color(&text_city1_layer, GColorWhite);
+  text_layer_set_background_color(&text_city1_layer, GColorClear);
+  layer_set_frame(&text_city1_layer.layer, GRect(10, 4, 144-10, 168-4));
+  text_layer_set_font(&text_city1_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));
+  layer_add_child(&window.layer, &text_city1_layer.layer);
+  text_layer_set_text(&text_city1_layer, CITY1);
 
-  // 1st date (San Francisco)
-  text_layer_init(&text_date1_layer, window.layer.frame);
-  text_layer_set_text_color(&text_date1_layer, GColorWhite);
-  text_layer_set_background_color(&text_date1_layer, GColorClear);
-  layer_set_frame(&text_date1_layer.layer, GRect(10, 4, 144-10, 168-4));
-  text_layer_set_font(&text_date1_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));
-  layer_add_child(&window.layer, &text_date1_layer.layer);
+  // 1st date
+  // text_layer_init(&text_date1_layer, window.layer.frame);
+  // text_layer_set_text_color(&text_date1_layer, GColorWhite);
+  // text_layer_set_background_color(&text_date1_layer, GColorClear);
+  // layer_set_frame(&text_date1_layer.layer, GRect(10, 4, 144-10, 168-4));
+  // text_layer_set_font(&text_date1_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));
+  // layer_add_child(&window.layer, &text_date1_layer.layer);
 
-  // 1st time (San Francisco)
+  // 1st time
   text_layer_init(&text_time1_layer, window.layer.frame);
   text_layer_set_text_color(&text_time1_layer, GColorWhite);
   text_layer_set_background_color(&text_time1_layer, GColorClear);
@@ -53,16 +67,25 @@ void handle_init(AppContextRef ctx) {
   text_layer_set_font(&text_time1_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_49)));
   layer_add_child(&window.layer, &text_time1_layer.layer);
 
-	
-  // 2nd date (Berlin)
-  text_layer_init(&text_date2_layer, window.layer.frame);
-  text_layer_set_text_color(&text_date2_layer, GColorWhite);
-  text_layer_set_background_color(&text_date2_layer, GColorClear);
-  layer_set_frame(&text_date2_layer.layer, GRect(10, 88, 144-10, 168-88));
-  text_layer_set_font(&text_date2_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));
-  layer_add_child(&window.layer, &text_date2_layer.layer);
 
-  // 2nd time (Berlin)
+  // 2nd city
+  text_layer_init(&text_city2_layer, window.layer.frame);
+  text_layer_set_text_color(&text_city2_layer, GColorWhite);
+  text_layer_set_background_color(&text_city2_layer, GColorClear);
+  layer_set_frame(&text_city2_layer.layer, GRect(10, 88, 144-10, 168-88));
+  text_layer_set_font(&text_city2_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));
+  layer_add_child(&window.layer, &text_city2_layer.layer);
+  text_layer_set_text(&text_city2_layer, CITY2);
+
+  // 2nd date
+  // text_layer_init(&text_date2_layer, window.layer.frame);
+  // text_layer_set_text_color(&text_date2_layer, GColorWhite);
+  // text_layer_set_background_color(&text_date2_layer, GColorClear);
+  // layer_set_frame(&text_date2_layer.layer, GRect(10, 88, 144-10, 168-88));
+  // text_layer_set_font(&text_date2_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));
+  // layer_add_child(&window.layer, &text_date2_layer.layer);
+
+  // 2nd time
   text_layer_init(&text_time2_layer, window.layer.frame);
   text_layer_set_text_color(&text_time2_layer, GColorWhite);
   text_layer_set_background_color(&text_time2_layer, GColorClear);
@@ -91,8 +114,8 @@ void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
 
   // TODO: Only update the date when it's changed.
   string_format_time(date_text, sizeof(date_text), "%B %e", t->tick_time);
-  text_layer_set_text(&text_date1_layer, date_text);
-  text_layer_set_text(&text_date2_layer, date_text);
+  // text_layer_set_text(&text_date1_layer, date_text);
+  // text_layer_set_text(&text_date2_layer, date_text);
 
 
   if (clock_is_24h_style()) {
